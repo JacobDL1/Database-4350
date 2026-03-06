913732d3d914380a4e69a574d632dc8ca55c377a
JacobDL1 <JacobLamb3@my.unt.edu>
2026-03-06 17:25:56 -0600

    Update database.py to be utilize functions for modularity and clearer flow

--- database.py
+++ database.py
@@ -1,10 +1,9 @@
 import os #used for os.path.exists method that updates dbValues to have all entries data.db does
 import sys #used for sys.stoud.flush(), addressed gradebot issue
 
-def main():
-  file = "data.db" #used to reference data.db later
-  dbValues = [] #stores all set command key value pairs for faster searching when using get
+file = "data.db" #used to reference data.db later
 
+def loadDB(dbValues):
   if os.path.exists(file): #if data.db exists, each line is read, and the key value pair is appended to dbValues
     with open(file, "r") as f:
       for i in f:
@@ -12,74 +11,66 @@ def main():
 
         if len(dbEntry) < 3: #skip empty or miswritten lines from gradebot
           continue
+        duplicate = False #tracks if duplicate entry is found while importing data.db information into dbValues
         
-        importDuplicate = False #tracks if duplicate entry is found while importing data.db information into dbValues
-
         for j in dbValues: #compares key value of new line from data.db stored in dbEntry to those already in dbValues, ensuring that any duplicates from data.db are rooted out
           if j[0] == dbEntry[1]:
             j[1] = dbEntry[2]
-            importDuplicate = True
+            duplicate = True
             break
             
-        if not importDuplicate:
+        if not duplicate:
           dbValues.append([dbEntry[1].strip(), dbEntry[2].strip()]) #dbEntry[0] is SET, so following key value pairs and index 1 and 2 is appended to dbValues
+
+def setKeyValue(dbValues, key, value):
+  duplicate = False
+  for i in dbValues: #checks if new key is already present in dbValues, and updates key's value in dbValues if there is a duplicate
+    if i[0] == key:
+      i[1] = value
+      duplicate = True
+      break
+      
+  if not duplicate:
+    dbValues.append([key, value])
+    
+  with open(file, "a") as f: #opens and writes to data.db
+    f.write(f"SET {key} {value}\n")
+    f.flush() #used for making sure SET is complete by the time GET is used for gradebot
+    os.fsync(f.fileno()) #used for making sure SET is complete by the time GET is used for gradebot
+  print("OK") #confirmation of SET command's success
+
+def getKeyValue(dbValues, key):
+  foundValue = ""
   
-  while(True): #continues looping until broken allowing for contuninous input
-    set = False #tracks if user typed set
-    get = False #tracks if user typed get
+  for i in dbValues: #comapres user-provided key to entries in dbValues, prints value if key is found
+    if i[0] == key:
+      foundValue = (i[1])
+  if foundValue != "":
+    print(foundValue)
+
+def main():
+  dbValues = [] #stores all set command key value pairs for faster searching when using get
+  loadDB(dbValues)
+  
+  while True: #continues looping until broken allowing for contuninous input
     try:
       userInput = input()
     except EOFError:
       break
+      
     userInput = userInput.strip() #removes leading/trailing spaces for accurate indexing
     words = userInput.split(" ", 2) #splits userInput into its individual words, maxes out at two splits to make sure users can enter values that have spaces
     
     if words[0].upper() == "SET": #upper is used to make sure if people type any command in lowercase the program still recognizes the intent of the user
-      set = True
+      setKeyValue(dbValues, words[1].strip(), words[2].strip())
     elif words[0].upper() == "GET":
-      get = True
+      getKeyValue(dbValues, words[1].strip())
     elif words[0].upper() == "EXIT":
       break
     else:
       print("Invalid command entered")
       
-    key = ""
-    value = ""
-    duplicate = False #used to track if a SET request's key already exists
-    
-    if (set): #if  user used SET, key value pair is appended to dbValues and written to data.db, and if key is a duplicate, existing key has its value changed to new SET request's value
-      key = words[1].strip()
-      value = words[2].strip()
-      
-      for i in dbValues: #checks if new key is already present in dbValues, and updates key's value in dbValues if there is a duplicate
-        if i[0] == key:
-          i[1] = value
-          duplicate = True
-          break
-          
-      if not duplicate:
-        dbValues.append([key, value])
-      
-      with open(file, "a") as f: #opens and writes to data.db
-        f.write(f"SET {key} {value}\n")
-        f.flush() #used for making sure SET is complete by the time GET is used for gradebot
-        os.fsync(f.fileno()) #used for making sure SET is complete by the time GET is used for gradebot
-
-      print("OK") #confirmation of SET command's success
-    
-    elif (get): #if user used GET, key is searched for in dbValues and returned if found
-      key = words[1].strip()
-      foundValue = ""
-      
-      for i in dbValues: #comapres user-provided key to entries in dbValues, prints value if key is found
-        if i[0] == key:
-          foundValue = (i[1])
-      if foundValue != "":
-        print(foundValue)
-
-        
     sys.stdout.flush()
           
-
 if __name__ == "__main__":
   main()
