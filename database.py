64716bb688f76dbe8f191538ac19c52f226895fc
JacobDL1 <JacobLamb3@my.unt.edu>
2026-03-06 14:22:54 -0600

    Update database.py so that the SET method checks for duplicates before appending key value pairs to the dbValues array and data.db file

--- database.py
+++ database.py
@@ -16,6 +16,7 @@ def main():
     userInput = input()
     userInput = userInput.strip() #removes leading/trailing spaces for accurate indexing
     words = userInput.split(" ", 2) #splits userInput into its individual words, maxes out at two splits to make sure users can enter values that have spaces
+    
     if words[0] == "SET":
       set = True
     elif words[0] == "GET":
@@ -28,17 +29,28 @@ def main():
       
     key = ""
     value = ""
+    duplicate = False #used to track if a SET request's key already exists
     
-    if (set): #if  user used SET, key value pair is appended to dbValues and written to data.db
+    if (set): #if  user used SET, key value pair is appended to dbValues and written to data.db, and if key is a duplicate, existing key has its value changed to new SET request's value
       key = words[1]
       value = words[2]
-      dbValues.append([key, value])
+      
+      for i in dbValues:
+        if i[0] == key:
+          i[1] = value
+          duplicate = True
+          break
+          
+      if not duplicate:
+        dbValues.append([key, value])
       
       with open(file, "a") as f: #opens and writes to data.db
         f.write(f"SET {key} {value}\n")
+    
     elif (get): #if user used GET, key is searched for in dbValues and returned if found
       key = words[1]
       foundValue = "";
+      
       for i in dbValues:
         if i[0] == key:
           foundValue = (i[1])
