d4dcee3513b6eb0c130a598561bbb4d0a032a3f0
JacobDL1 <JacobLamb3@my.unt.edu>
2026-03-06 12:49:23 -0600

    Update database.py to fix true/false syntax (didn't capitalize first letter), and fixed userInput indexing by assigning words to userInput.split(). I was addressing individual characters before. dbValues is moved outside of the while loop now so it retains values

--- database.py
+++ database.py
@@ -1,35 +1,36 @@
 def main():
   file = "data.db" #used to reference data.db later
+  dbValues = [] #stores all set command key value pairs for faster searching when using get
   
   
-  while(true): #continues looping until broken allowing for contuninous input
-    set = false #tracks if user typed set
-    get = false #tracks if user typed get
+  while(True): #continues looping until broken allowing for contuninous input
+    set = False #tracks if user typed set
+    get = False #tracks if user typed get
     userInput = input()
     userInput = userInput.strip() #removes leading/trailing spaces for accurate indexing
-    if userInput[0] == "SET":
-      set = true
-    elif userInput[0] == "GET":
-      get = true
-    elif userInput[0] == "EXIT":
+    words = userInput.split() #splits userInput into its individual words
+    if words[0] == "SET":
+      set = True
+    elif words[0] == "GET":
+      get = True
+    elif words[0] == "EXIT":
       print(exiting)
       break;
     else:
       print("Invalid command entered")
       
-    dbValues = [] #stores all set command key value pairs for faster searching when using get
     key = ""
     value = ""
     
     if (set): #if  user used SET, key value pair is appended to dbValues and written to data.db
-      userInput[1] = key
-      userInput[2] = value
+      words[1] = key
+      words[2] = value
       dbValues.append([key, value])
       
       with open(DB_FILE, "a") as f: #opens and writes to data.db
         f.write(f"SET {key} {value}\n")
     elif (get): #if user used GET, key is searched for in dbValues and returned if found
-      userInput[1] = key
+      words[1] = key
       for i in dbValues:
         if i == key:
           return i[1]
