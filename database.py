6a6493b2763bf0509877bba96c0fff00af1790ef
JacobDL1 <JacobLamb3@my.unt.edu>
2026-03-06 18:01:23 -0600

    Update database.py, pawned off duplicate checks on a new function to try and create more uniformity in duplication checks

--- database.py
+++ database.py
@@ -12,29 +12,23 @@ def loadDB(dbValues):
 
         if len(dbEntry) < 3: #skip empty or miswritten lines from gradebot
           continue
-        duplicate = False #tracks if duplicate entry is found while importing data.db information into dbValues
-        
-        for j in dbValues: #compares key value of new line from data.db stored in dbEntry to those already in dbValues, ensuring that any duplicates from data.db are rooted out
-          if j[0] == dbEntry[1]:
-            j[1] = dbEntry[2]
-            duplicate = True
-            break
-            
-        if not duplicate:
-          dbValues.append([dbEntry[1].strip(), dbEntry[2].strip()]) #dbEntry[0] is SET, so following key value pairs and index 1 and 2 is appended to dbValues
+          
+        key = dbEntry[1]
+        value = dbEntry[2]
+        duplicateCheck(dbValues, key, value)
+
+def duplicateCheck(dbValues, key, value):
+  """Updates key's value if there is a duplicate key in dbValues, if there is no duplication, key-value pair is appended"""
+  for i in dbValues: #compares key value of new line from data.db stored in dbEntry to those already in dbValues, ensuring that any duplicates from data.db are rooted out
+    if i[0] == key:
+      i[1] = value
+      return
+  dbValues.append([key, value])
 
 def setKeyValue(dbValues, key, value):
   """if the user types SET, stores following two terms as a key value pair in both dbValues and data.db"""
-  duplicate = False
-  for i in dbValues: #checks if new key is already present in dbValues, and updates key's value in dbValues if there is a duplicate
-    if i[0] == key:
-      i[1] = value
-      duplicate = True
-      break
-      
-  if not duplicate:
-    dbValues.append([key, value])
-    
+  duplicateCheck(dbValues, key, value)
+  
   with open(file, "a") as f: #opens and writes to data.db
     f.write(f"SET {key} {value}\n")
     f.flush() #used for making sure SET is complete by the time GET is used for gradebot
@@ -43,13 +37,11 @@ def setKeyValue(dbValues, key, value):
 
 def getKeyValue(dbValues, key):
   """if the user types GET, searched through dbValues for the user specified key and returns the corresponding value if the key is found"""
-  foundValue = ""
-  
   for i in dbValues: #comapres user-provided key to entries in dbValues, prints value if key is found
     if i[0] == key:
-      foundValue = (i[1])
-  if foundValue != "":
-    print(foundValue)
+      print(i[1])
+      return
+  print("NULL") #catches missing keys
 
 def main():
   """main loop for taking in user input and calling the necessary functions to respond"""
