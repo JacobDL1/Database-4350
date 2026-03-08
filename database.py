9b88fd10544173e68879ee6d7117a8f89094ee60
Jacob Lamb <JacobLamb3@my.unt.edu>
2026-03-08 15:19:46 -0500

    Improved docstrings and comments (some comments were very lengthy or unclear)

--- database.py
+++ database.py
@@ -1,17 +1,17 @@
-import os #used for os.path.exists method that updates dbValues to have all entries data.db does
-import sys #used for sys.stdout.flush(), addressed gradebot issue
+import os #used for os.path.exists()
+import sys #used for sys.stdout.flush()
 
-FILE = "data.db" #used to reference data.db later
+FILE = "data.db"
 
 def loadDB(dbValues):
-    """reads from data.db and takes any infromation present and uses it to rebuild the in-memory index (dbValues)"""
+    """Uses data.db to rebuild dbValues for memory persistence"""
     dbValues.clear()
     if os.path.exists(FILE): #if data.db exists, each line is read, and the key value pair is appended to dbValues
         try:
             with open(FILE, "r", encoding="utf-8") as f: #opens using utf-8 encoding to parse file properly
                 for i in f:
-                    dbEntry = i.strip().replace('\r', '').split(maxsplit=2) #removes leading and trailing spaces, then splits the entry based on spaces, with a max of two splits, use decode on i to make python accurately interpret text for compatibility with the grader
-                    if len(dbEntry) < 3 or dbEntry[0] != "SET": #skip empty or miswritten lines from gradebot
+                    dbEntry = i.strip().replace('\r', '').split(maxsplit=2) #strip and split at most 2 times, replace all '\r' with nothing
+                    if len(dbEntry) < 3 or dbEntry[0] != "SET": #skip empty or miswritten lines
                         continue
                     key = dbEntry[1]
                     value = dbEntry[2]
@@ -20,15 +20,15 @@ def loadDB(dbValues):
             print(f"Error when reading database: {e}", file=sys.stderr)
 
 def duplicateCheck(dbValues, key, value):
-    """Updates key's value if there is a duplicate key in dbValues, if there is no duplication, key-value pair is appended"""
-    for i in dbValues: #compares key value of new line from data.db stored in dbEntry to those already in dbValues, ensuring that any duplicates from data.db are rooted out
+    """Appends new entry or updates existing key, uses last write wins"""
+    for i in dbValues: #compares key value from data.db to those already in dbValues, only appends unique keys
         if i[0] == key:
             i[1] = value
             return
     dbValues.append([key, value])
 
 def setKeyValue(dbValues, key, value):
-    """if the user types SET, stores following two terms as a key value pair in both dbValues and data.db"""
+    """Updates dbValues with key-value pair and adds entry to data.db"""
     duplicateCheck(dbValues, key, value)
 
     try:
@@ -40,9 +40,9 @@ def setKeyValue(dbValues, key, value):
         print(f"Error when writing database: {e}", file=sys.stderr)
 
 def getKeyValue(dbValues, key):
-    """compares user-dictated key against the keys in dbValues, clears and reloads dbValues right before checking to ensure all entries are present"""
+    """Update dbValues for accuracy, then print value of key"""
     loadDB(dbValues)
-    for i in dbValues: #comapres user-provided key to entries in currValues, prints value if key is found, used because relying on dbValues created errors with grader
+    for i in dbValues: #comapres key to keys in currValues, prints associated value if found
         if i[0] == key:
             print(i[1])
             sys.stdout.flush()
@@ -51,24 +51,24 @@ def getKeyValue(dbValues, key):
     sys.stdout.flush()
 
 def main():
-    """main loop for taking in user input and calling the necessary functions to respond"""
-    dbValues = [] #stores all set command key value pairs for faster searching when using get
+    """Loops to take user input, calls necessary functions to respond"""
+    dbValues = [] #stores all set command key value pairs
     loadDB(dbValues)
 
-    while True: #continues looping until broken allowing for contuninous input
+    while True:
         userInput = sys.stdin.buffer.readline() #reads raw bytes instead of parsing using input() for more accurate input
 
         if not userInput: #EOF detection
             break
-        userInput = userInput.decode("utf-8", errors = "replace").strip().replace("\r", "") #decodes user input
+        userInput = userInput.decode("utf-8", errors = "replace").strip().replace("\r", "") #decodes user input then strips user input
 
         if not userInput: #skip blank lines
             continue
-        words = userInput.split(maxsplit=2) #splits userInput into its individual words, maxes out at two splits to make sure users can enter values that have spaces
+        words = userInput.split(maxsplit=2) #split at most 2 times
 
-        if words[0].upper() == "SET" and len(words) >= 3: #upper makes user input case insensitive, words is at least 3 words because that covers the command, key, and the value, where the value can include its own spaces
+        if words[0].upper() == "SET" and len(words) >= 3: #use upper so input is case insensitive, make sure there aren't too many arguements
             setKeyValue(dbValues, words[1], words[2])
-        elif words[0].upper() == "GET" and len(words) >= 2: #at least 2 since there needs to be the command and the key
+        elif words[0].upper() == "GET" and len(words) >= 2:
             getKeyValue(dbValues, words[1])
         elif words[0].upper() == "EXIT":
             break
